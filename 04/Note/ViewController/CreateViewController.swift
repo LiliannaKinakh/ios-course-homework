44a367d725e86a78ade5e5f80579029fd66ef9dc
Vladyslav Zhulavskyi <vladyslavzhulavskyi@MacBook-Air-Vladyslav.local>
2018-11-23 14:31:48 +0200

    DZ6.1
    Add border width/radius/color to save button, swap position edit/add, fix constraint

--- 04/Note/ViewController/CreateViewController.swift
+++ 04/Note/ViewController/CreateViewController.swift
@@ -14,13 +14,23 @@ class CreateViewController: UIViewController {
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var textLabel: UILabel!
     @IBOutlet weak var textTextField: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         titleLabel.text = "Title"
         textLabel.text = "Text"
+        attributeToSaveButton()
+        
     }
-    @IBAction func SaveDiary(_ sender: Any) {
+    func attributeToSaveButton (){
+        saveButton.layer.borderWidth = 1
+        saveButton.layer.borderColor = UIColor.blue.cgColor
+        saveButton.layer.cornerRadius = saveButton.bounds.height / 2.5
+        
     }
+    @IBAction func SaveButton(_ sender: Any) {
+    }
+    
 }
